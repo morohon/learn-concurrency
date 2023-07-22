c784e99e54a9c7505d28d9efcfab9f75824b5bab
Kirill Verevkin <kira795@yandex.ru>
2023-07-22 16:50:43 +0300

    Реализовал неизменяемый класс Order и убрал синхронизацию

--- src/main/java/course/concurrency/m3_shared/immutable/Order.java
+++ src/main/java/course/concurrency/m3_shared/immutable/Order.java
@@ -1,22 +1,33 @@
 package course.concurrency.m3_shared.immutable;
 
+import java.util.Collections;
 import java.util.List;
 
 import static course.concurrency.m3_shared.immutable.Order.Status.NEW;
 
 public class Order {
 
-    public enum Status { NEW, IN_PROGRESS, DELIVERED }
+    public enum Status { NEW, IN_PROGRESS, DELIVERED;}
+    private final Long id;
 
-    private Long id;
-    private List<Item> items;
-    private PaymentInfo paymentInfo;
-    private boolean isPacked;
-    private Status status;
+    private final List<Item> items;
+    private final PaymentInfo paymentInfo;
+    private final boolean isPacked;
+    private final Status status;
+    public Order(Long id, List<Item> items, PaymentInfo paymentInfo, boolean isPacked, Status status) {
+        this.id = id;
+        this.items = Collections.unmodifiableList(items);
+        this.paymentInfo = paymentInfo;
+        this.isPacked = isPacked;
+        this.status = status;
+    }
 
-    public Order(List<Item> items) {
-        this.items = items;
-        this.status = NEW;
+    public Order withPaymentInfo(PaymentInfo paymentInfo) {
+        return new Order(this.id, this.items, paymentInfo, this.isPacked, this.status);
+    }
+
+    public Order withStatus(Status status) {
+        return new Order(this.id, this.items, this.paymentInfo, this.isPacked, status);
     }
 
     public synchronized boolean checkStatus() {
@@ -30,10 +41,6 @@ public class Order {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public List<Item> getItems() {
         return items;
     }
@@ -42,25 +49,16 @@ public class Order {
         return paymentInfo;
     }
 
-    public void setPaymentInfo(PaymentInfo paymentInfo) {
-        this.paymentInfo = paymentInfo;
-        this.status = Status.IN_PROGRESS;
-    }
-
     public boolean isPacked() {
         return isPacked;
     }
 
-    public void setPacked(boolean packed) {
-        isPacked = packed;
-        this.status = Status.IN_PROGRESS;
+    public Order withPacked(boolean packed) {
+        return new Order(this.id, this.items, this.paymentInfo, packed, Status.IN_PROGRESS);
     }
 
     public Status getStatus() {
         return status;
     }
 
-    public void setStatus(Status status) {
-        this.status = status;
-    }
 }
